2667e4005ff1d94168915c15e01c1d90dfe5c9d2
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-04-26 19:23:50 -0700

    add command line tools

--- addPrivateRepos.py
+++ addPrivateRepos.py
@@ -1,11 +1,23 @@
+"""Usage: addPrivateRepos.py <name> ...
+          addPrivateRepos.py (-u <username> -p <password> -t <team-id> -o <owners-team-id> -O <org>) <name> ...
+
+Options:
+  -h --help
+  -u --username         GitHub API username
+  -p --password         API password
+  -t --team-id          Team Id
+  -o --owners-team-id   Owners Team Id
+  -O --org              GitHub Organization
+"""
+
 from pygithub3 import Github
-from secrets import *
+from docopt import docopt
 
 """
  README
 
 * Install https://github.com/copitux/python-github3
-   'pip install pygithub3'
+   'pip install pygithub3 docopt'
 * Create a secrets.py file with the following:
 	USERNAME="you"  # must be an AI owner to run this script
 	PASSWORD="sekrit"
@@ -14,9 +26,22 @@ from secrets import *
 	ORG="AdaInitiative"
 """
 
-# ADD YOUR LIST OF NAMES HERE
-# TODO: make this better, I'll set up argparse in v0.2 unless someone has a better idea
-usernames = ["username1", "username2"]
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    # if username in args, use command line params
+    # docopt will give usage if you try to pass a username and not the rest of the required params
+    if arguments['<username>']:
+        USERNAME = arguments['<username>']
+        PASSWORD = arguments['<password>']
+        TEAM_ID =  arguments['<team-id>']
+        OWNERS =   arguments['<owners-team-id>']
+        ORG =      arguments['<org>']
+    # otherwise, use secrets.py
+    else:
+        from secrets import *
+    # use names from command line no matter what
+    usernames = arguments['<name>']
+    print usernames
 
 gh = Github(login=USERNAME, password=PASSWORD)
 org = ORG
@@ -67,3 +92,4 @@ for user in usernames:
 	print "Added %s to team %s" % (user,user)
 
 print "\n== ALL DONE! =="
+
